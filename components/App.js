e69f5abb96944677e7e3a706582a01f0dd513845
benice195 <szczyrbaw@gmail.com>
2017-12-26 11:24:43 +0100

    code review

--- components/App.js
+++ components/App.js
@@ -44,23 +44,7 @@ App = React.createClass({
       })
   },
   
-  getGif: function(searchingText, callback) {
-    var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
-    xhr.onload = function() {
-      if (xhr.status === 200) {
-        var data = JSON.parse(xhr.responseText).data;
-        var gif = {
-          url: data.fixed_width_downsampled_url,
-          sourceUrl: data.url
-        };
-        callback(gif);
-      }
-    };
-    xhr.send();
-  },
-  
+
   render: function() {
     var styles = {
       margin: '0 auto',
